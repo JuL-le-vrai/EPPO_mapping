d382425c5977f662c9956964b29b3938fe82273e
Jules Vandeputte - Machine INRIA <jules.vandeputte@inria.fr>
2024-11-29 10:35:09 +0100

    Adding methods used in the paper

--- scripts/Extract_mapping_V2.py
+++ scripts/Extract_mapping_V2.py
@@ -28,7 +28,13 @@ if __name__ == '__main__':
     parser.add_argument('--method', default='EditionDistance', choices=['GPT-embed', 
                                                                         'GPT-embed-ExpertPrompt',
                                                                         'EditionDistanceIndel',
-                                                                        'GPT-embed-ExpertPrompt2'])
+                                                                        'GPT-embed-ExpertPrompt3',
+                                                                        'GPT-embed-relative-cosim',
+                                                                        'HandmapedOnly',
+                                                                        'GPT-HybridHandmaped',
+                                                                        'Pllama-embed',
+                                                                        'Llama3-embed',
+                                                                        'Levenshtein'])
     parser.add_argument('--ds-list', type=list_of_strings, default='PN,PV,Bousset,IPM')
     parser.add_argument('--drctn', type=str, default = 'EPPOtoDS', choices=['EPPOtoDS', 'DStoEPPO'])
     args = parser.parse_args()
@@ -107,6 +113,106 @@ if __name__ == '__main__':
                                     dict_codes[code]['mapsto'][ds].add((subid, ratio))
                                     mapping[ds][subid].append((code, ratio))
 
+    if args.method == 'Levenshtein':
+
+        mapping = { ds : {} for ds in args.ds_list}
+
+        for code in tqdm(dict_codes):
+            for name1 in dict_codes[code]['names']:
+                if name1['active'] == 'true':
+                    # standardizing the names with the same standard
+                    standard_name1 = name1['full_name'].lower()
+                    standard_name1 = ''.join(x for x in standard_name1.title() if x.isalnum()) 
+
+                    for ds in args.ds_list:
+                        for subid in dic_possible_names[ds]:
+                            if subid not in mapping[ds]:
+                                mapping[ds][subid] = []
+                            for name2 in dic_possible_names[ds][subid]:
+                                # standardizing the names with the same standard
+                                standard_name2 = name2.lower()
+                                standard_name2 = ''.join(x for x in standard_name2.title() if x.isalnum())
+                                # Computing the edition distance
+                                ratio = rapidfuzz.distance.Levenshtein.normalized_similarity(standard_name1, standard_name2, score_cutoff = tolerance)
+                                if ratio >= tolerance:
+                                    dict_codes[code]['mapsto'][ds].add((subid, ratio))
+                                    mapping[ds][subid].append((code, ratio))
+
+
+
+                                    
+
+    if args.method == 'HandmapedOnly':
+
+        mapping = { ds : {} for ds in args.ds_list}
+        for ds in args.ds_list:
+            df = pd.read_csv(f'../data/Extract_EPPO/EPPO_API_results_{ds}.csv', sep = ';')
+            for _, row in df.iterrows():
+                if row['EPPO_final'] != '****NOT FOUND*****':
+                    if row['iName'] not in mapping[ds]:
+                        mapping[ds][row['iName']] = []
+                    mapping[ds][row['iName']].append((row['EPPO_final'], 101))
+                    dict_codes[row['EPPO_final']]['mapsto'][ds].add((str(row['iName']), 101))
+
+
+
+    if args.method == 'GPT-HybridHandmaped':
+
+        mapping = { ds : {} for ds in args.ds_list}
+        for ds in args.ds_list:
+            df = pd.read_csv(f'../data/Extract_EPPO/EPPO_API_results_{ds}.csv', sep = ';')
+            for _, row in df.iterrows():
+                if row['EPPO_final'] != '****NOT FOUND*****':
+                    if row['iName'] not in mapping[ds]:
+                        mapping[ds][row['iName']] = []
+                    mapping[ds][row['iName']].append((row['EPPO_final'], 101))
+                    dict_codes[row['EPPO_final']]['mapsto'][ds].add((str(row['iName']), 101))
+
+
+        cfg = SimpleNamespace(**{})
+        cfg.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        cosim = torch.nn.CosineSimilarity(dim=1)
+
+        considered_lang = ['la', 'en', 'fr']
+        dic_names_EPPO = {}
+        for code in dict_codes:
+            dic_names_EPPO[code] = [name['full_name'].lower().strip(' _').replace('_',' ').replace('.','') for name in dict_codes[code]['names'] if name['lang'] in considered_lang and name['full_name'] != None and name['active'] == 'true']
+
+        # Loading the precomputed tensors for the different names of each EPPO code
+        list_tensors = []
+        list_EPPO_codes = []
+        for EPPO_code in dic_names_EPPO:
+            for EPPO_name in dic_names_EPPO[EPPO_code]:
+                EPPO_name = EPPO_name.replace('/','')          
+                list_tensors.append(torch.load(f'../data/Extract_EPPO/GPT_features/EPPO/{EPPO_code}/{EPPO_name}.pt'))
+                list_EPPO_codes.append(EPPO_code)
+        tensor_EPPO = torch.Tensor(list_tensors)
+
+        # For each dataset, for each subid and each name, loading the precomputed tensors 
+        dic_tensors_DS = {}
+        for ds in args.ds_list:
+            dic_tensors_DS[ds] = {}
+            for subid in dic_possible_names[ds]:
+                dic_tensors_DS[ds][subid] = {}
+                for name in dic_possible_names[ds][subid]:
+                    name = name.replace('/','').replace('_',' ')      
+                    if name != '':
+                        dic_tensors_DS[ds][subid][name] = torch.Tensor(torch.load(f'../data/Extract_EPPO/GPT_features/{ds}/{subid}/{name}.pt'))
+
+        for ds in dic_tensors_DS:
+            for subid in tqdm(dic_tensors_DS[ds]):
+                if subid not in mapping[ds]:
+                    mapping[ds][subid] = []
+                for name in dic_tensors_DS[ds][subid]:
+                    # Defining the similarity function used to compare tensors : Cosine similarity
+                    sim = cosim(dic_tensors_DS[ds][subid][name], tensor_EPPO)
+                    # Finding the index of the EPPO code that maximizes then similarity (cosine similarity)
+                    i_max = torch.argmax(sim).item()
+                    if sim[i_max].item() >= tolerance:
+                        EPPO_code = list_EPPO_codes[i_max]
+                        dict_codes[EPPO_code]['mapsto'][ds].add((subid, sim[i_max].item()))
+                        mapping[ds][subid].append((EPPO_code, sim[i_max].item()))
+
 
 
     if args.method == 'GPT-embed':
@@ -157,6 +263,212 @@ if __name__ == '__main__':
                         dict_codes[EPPO_code]['mapsto'][ds].add((subid, sim[i_max].item()))
                         mapping[ds][subid].append((EPPO_code, sim[i_max].item()))
 
+
+    if args.method == 'Pllama-embed':
+
+        cfg = SimpleNamespace(**{})
+        cfg.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        cosim = torch.nn.CosineSimilarity(dim=1)
+
+        mapping = { ds : {} for ds in args.ds_list}
+
+        considered_lang = ['la', 'en', 'fr']
+        dic_names_EPPO = {}
+        for code in dict_codes:
+            dic_names_EPPO[code] = [name['full_name'].lower().strip(' _').replace('_',' ').replace('.','').replace('\'','') for name in dict_codes[code]['names'] if name['lang'] in considered_lang and name['full_name'] != None and name['active'] == 'true']
+
+        # Loading the precomputed tensors for the different names of each EPPO code
+        list_tensors = []
+        list_EPPO_codes = []
+        for EPPO_code in dic_names_EPPO:
+            for EPPO_name in dic_names_EPPO[EPPO_code]:
+                EPPO_name = EPPO_name.replace('/','')          
+                list_tensors.append(torch.load(f'../data/Extract_EPPO/PLLaMa-7b-base_features/EPPO/{EPPO_code}/{EPPO_name}.pt')[0].tolist())
+                list_EPPO_codes.append(EPPO_code)
+        # print(list_tensors[0])
+        tensor_EPPO = torch.Tensor(list_tensors)
+
+        # For each dataset, for each subid and each name, loading the precomputed tensors 
+        dic_tensors_DS = {}
+        for ds in args.ds_list:
+            dic_tensors_DS[ds] = {}
+            for subid in dic_possible_names[ds]:
+                dic_tensors_DS[ds][subid] = {}
+                for name in dic_possible_names[ds][subid]:
+                    name = name.replace('/','').replace('_',' ')
+                    if name != '':
+                        dic_tensors_DS[ds][subid][name] = torch.Tensor(torch.load(f'../data/Extract_EPPO/PLLaMa-7b-base_features/{ds}/{subid}/{name}.pt'))
+
+        for ds in dic_tensors_DS:
+            for subid in tqdm(dic_tensors_DS[ds]):
+                if subid not in mapping[ds]:
+                    mapping[ds][subid] = []
+                for name in dic_tensors_DS[ds][subid]:
+                    # Defining the similarity function used to compare tensors : Cosine similarity
+                    sim = cosim(dic_tensors_DS[ds][subid][name], tensor_EPPO)
+                    # Finding the index of the EPPO code that maximizes then similarity (cosine similarity)
+                    i_max = torch.argmax(sim).item()
+                    if sim[i_max].item() >= tolerance:
+                        EPPO_code = list_EPPO_codes[i_max]
+                        dict_codes[EPPO_code]['mapsto'][ds].add((subid, sim[i_max].item()))
+                        mapping[ds][subid].append((EPPO_code, sim[i_max].item()))
+
+
+    if args.method == 'Llama3-embed':
+
+        cfg = SimpleNamespace(**{})
+        cfg.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        cosim = torch.nn.CosineSimilarity(dim=1)
+
+        mapping = { ds : {} for ds in args.ds_list}
+
+        considered_lang = ['la', 'en', 'fr']
+        dic_names_EPPO = {}
+        for code in dict_codes:
+            dic_names_EPPO[code] = [name['full_name'].lower().strip(' _').replace('_',' ').replace('.','').replace('\'','') for name in dict_codes[code]['names'] if name['lang'] in considered_lang and name['full_name'] != None and name['active'] == 'true']
+
+        # Loading the precomputed tensors for the different names of each EPPO code
+        list_tensors = []
+        list_EPPO_codes = []
+        for EPPO_code in dic_names_EPPO:
+            for EPPO_name in dic_names_EPPO[EPPO_code]:
+                EPPO_name = EPPO_name.replace('/','')          
+                list_tensors.append(torch.load(f'../data/Extract_EPPO/Meta-Llama-3.1-8B_features/EPPO/{EPPO_code}/{EPPO_name}.pt')[0].tolist())
+                list_EPPO_codes.append(EPPO_code)
+        # print(list_tensors[0])
+        tensor_EPPO = torch.Tensor(list_tensors)
+
+        # For each dataset, for each subid and each name, loading the precomputed tensors 
+        dic_tensors_DS = {}
+        for ds in args.ds_list:
+            dic_tensors_DS[ds] = {}
+            for subid in dic_possible_names[ds]:
+                dic_tensors_DS[ds][subid] = {}
+                for name in dic_possible_names[ds][subid]:
+                    name = name.replace('/','').replace('_',' ')
+                    if name != '':
+                        dic_tensors_DS[ds][subid][name] = torch.Tensor(torch.load(f'../data/Extract_EPPO/Meta-Llama-3.1-8B_features/{ds}/{subid}/{name}.pt'))
+
+        for ds in dic_tensors_DS:
+            for subid in tqdm(dic_tensors_DS[ds]):
+                if subid not in mapping[ds]:
+                    mapping[ds][subid] = []
+                for name in dic_tensors_DS[ds][subid]:
+                    # Defining the similarity function used to compare tensors : Cosine similarity
+                    sim = cosim(dic_tensors_DS[ds][subid][name], tensor_EPPO)
+                    # Finding the index of the EPPO code that maximizes then similarity (cosine similarity)
+                    i_max = torch.argmax(sim).item()
+                    if sim[i_max].item() >= tolerance:
+                        EPPO_code = list_EPPO_codes[i_max]
+                        dict_codes[EPPO_code]['mapsto'][ds].add((subid, sim[i_max].item()))
+                        mapping[ds][subid].append((EPPO_code, sim[i_max].item()))
+
+
+
+
+    if args.method == 'GPT-embed-ExpertPrompt3':
+
+        cfg = SimpleNamespace(**{})
+        cfg.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        cosim = torch.nn.CosineSimilarity(dim=1)
+
+        mapping = { ds : {} for ds in args.ds_list}
+
+        considered_lang = ['la', 'en', 'fr']
+        dic_names_EPPO = {}
+        for code in dict_codes:
+            dic_names_EPPO[code] = [name['full_name'].lower().strip(' _').replace('_',' ').replace('.','').replace('\'','') for name in dict_codes[code]['names'] if name['lang'] in considered_lang and name['full_name'] != None and name['active'] == 'true']
+
+        # Loading the precomputed tensors for the different names of each EPPO code
+        list_tensors = []
+        list_EPPO_codes = []
+        for EPPO_code in dic_names_EPPO:
+            for EPPO_name in dic_names_EPPO[EPPO_code]:
+                EPPO_name = EPPO_name.replace('/','')          
+                list_tensors.append(torch.load(f'../data/Extract_EPPO/GPT_features_expert_prompt3/EPPO/{EPPO_code}/{EPPO_name}.pt'))
+                list_EPPO_codes.append(EPPO_code)
+        print(list_tensors[0])
+        tensor_EPPO = torch.Tensor(list_tensors)
+
+        # For each dataset, for each subid and each name, loading the precomputed tensors 
+        dic_tensors_DS = {}
+        for ds in args.ds_list:
+            dic_tensors_DS[ds] = {}
+            for subid in dic_possible_names[ds]:
+                dic_tensors_DS[ds][subid] = {}
+                for name in dic_possible_names[ds][subid]:
+                    name = name.replace('/','').replace('_',' ')      
+                    if name != '':
+                        dic_tensors_DS[ds][subid][name] = torch.Tensor(torch.load(f'../data/Extract_EPPO/GPT_features_expert_prompt3/{ds}/{subid}/{name}.pt'))
+
+        for ds in dic_tensors_DS:
+            for subid in tqdm(dic_tensors_DS[ds]):
+                if subid not in mapping[ds]:
+                    mapping[ds][subid] = []
+                for name in dic_tensors_DS[ds][subid]:
+                    # Defining the similarity function used to compare tensors : Cosine similarity
+                    sim = cosim(dic_tensors_DS[ds][subid][name], tensor_EPPO)
+                    # Finding the index of the EPPO code that maximizes then similarity (cosine similarity)
+                    i_max = torch.argmax(sim).item()
+                    if sim[i_max].item() >= tolerance:
+                        EPPO_code = list_EPPO_codes[i_max]
+                        dict_codes[EPPO_code]['mapsto'][ds].add((subid, sim[i_max].item()))
+                        mapping[ds][subid].append((EPPO_code, sim[i_max].item()))
+
+
+
+    if args.method == 'GPT-embed-relative-cosim':
+
+        cfg = SimpleNamespace(**{})
+        cfg.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        cosim = torch.nn.CosineSimilarity(dim=1)
+
+        mapping = { ds : {} for ds in args.ds_list}
+
+        considered_lang = ['la', 'en', 'fr']
+        dic_names_EPPO = {}
+        for code in dict_codes:
+            dic_names_EPPO[code] = [name['full_name'].lower().strip(' _').replace('_',' ').replace('.','') for name in dict_codes[code]['names'] if name['lang'] in considered_lang and name['full_name'] != None and name['active'] == 'true']
+
+        # Loading the precomputed tensors for the different names of each EPPO code
+        list_tensors = []
+        list_EPPO_codes = []
+        for EPPO_code in dic_names_EPPO:
+            for EPPO_name in dic_names_EPPO[EPPO_code]:
+                EPPO_name = EPPO_name.replace('/','')          
+                list_tensors.append(torch.Tensor(torch.load(f'../data/Extract_EPPO/GPT_features/EPPO/{EPPO_code}/{EPPO_name}.pt')))
+                list_EPPO_codes.append(EPPO_code)
+        tensor_EPPO = torch.stack(list_tensors)
+
+        # For each dataset, for each subid and each name, loading the precomputed tensors 
+        dic_tensors_DS = {}
+        for ds in args.ds_list:
+            dic_tensors_DS[ds] = {}
+            for subid in dic_possible_names[ds]:
+                dic_tensors_DS[ds][subid] = {}
+                for name in dic_possible_names[ds][subid]:
+                    name = name.replace('/','').replace('_',' ')      
+                    if name != '':
+                        dic_tensors_DS[ds][subid][name] = torch.Tensor(torch.load(f'../data/Extract_EPPO/GPT_features/{ds}/{subid}/{name}.pt'))
+
+        for ds in dic_tensors_DS:
+            for subid in tqdm(dic_tensors_DS[ds]):
+                if subid not in mapping[ds]:
+                    mapping[ds][subid] = []
+                for name in dic_tensors_DS[ds][subid]:
+                    # Defining the similarity function used to compare tensors : Relative Cosine similarity
+                    n = 10
+                    sim = cosim(dic_tensors_DS[ds][subid][name], tensor_EPPO) # Compute cosim
+                    top_n_similarities, top_n_indices = torch.topk(sim, n, dim=0) # Top n
+                    sum_top_n = torch.sum(top_n_similarities, dim=0, keepdim=True)
+                    sim = sim / sum_top_n
+                    # Finding the index of the EPPO code that maximizes then similarity (cosine similarity)
+                    i_max = torch.argmax(sim).item()
+                    if sim[i_max].item() >= tolerance/n:
+                        EPPO_code = list_EPPO_codes[i_max]
+                        dict_codes[EPPO_code]['mapsto'][ds].add((subid, sim[i_max].item()))
+                        mapping[ds][subid].append((EPPO_code, sim[i_max].item()))
+
     # Replacing sets by lists to make the dict json parsable
     for code in dict_codes:
         for db in dict_codes[code]['mapsto']:
@@ -190,7 +502,7 @@ if __name__ == '__main__':
 
         final_mapping = {}
         for ds in mapping:
-            final_mapping[ds] = {subid : set() for subid in dic_possible_names[ds]}
+            final_mapping[ds] = {subid : [] for subid in dic_possible_names[ds]}
             for subid in mapping[ds]:
                 if len(mapping[ds][subid]) > 0:
                     sim = [i[1] for i in mapping[ds][subid]]
@@ -200,6 +512,16 @@ if __name__ == '__main__':
                     final_mapping[ds][subid] = []
 
 
+    if args.method =='HandmapedOnly':
+
+        final_mapping = {}
+        for ds in mapping:
+            final_mapping[ds] = {subid : [] for subid in dic_possible_names[ds]}
+            for subid in mapping[ds]:
+                if len(mapping[ds][subid]) > 0:
+                    final_mapping[ds][subid] = mapping[ds][subid][0]
+                else:
+                    final_mapping[ds][subid] = []
 
 
     with open(f'../data/Extract_EPPO/Harmonization_outputs/Mapping_{args.method}_{tolerance}_{args.drctn}_V2.json', 'w') as f:
